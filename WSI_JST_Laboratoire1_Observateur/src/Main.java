b3f18d0cd7db3f36e472f27713e32c2a6ed93646
SlWa99 <walid.slimani@hes-so.ch>
2024-02-22 14:48:52 +0100

    uml

--- WSI_JST_Laboratoire1_Observateur/src/Main.java
+++ WSI_JST_Laboratoire1_Observateur/src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Timer t = new Timer();
+        t.schedule()
+
     }
 }
